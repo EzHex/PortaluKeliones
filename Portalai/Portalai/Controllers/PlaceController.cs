aede92b159506b2daeaefa8ef36e05dd4109b72d
Arnas B <43886777+EzHex@users.noreply.github.com>
2023-03-20 18:46:40 +0200

    button in main screen for places

---
+++ Portalai/Portalai/Controllers/PlaceController.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Portalai.Models;
+
+namespace Portalai.Controllers;
+
+public class PlaceController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public PlaceController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
